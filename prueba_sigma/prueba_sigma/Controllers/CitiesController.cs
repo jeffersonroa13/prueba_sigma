5937b472eb30b2134887604882c4f9c36cb941c3
Jeff <jefferson.roa@unillanos.edu.co>
2020-08-12 18:48:59 -0500

    creacion y consumo de json para obtener departamentos y ciudades

---
+++ prueba_sigma/prueba_sigma/Controllers/CitiesController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using prueba_sigma.Infraestructure;
+using prueba_sigma.Models;
+using prueba_sigma.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace prueba_sigma.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CitiesController : Controller
+    {
+
+        [HttpGet("States")]
+        public async Task<CommandResult<List<string>>> GetStates()
+        {
+            DepartmentService service = new DepartmentService();
+            var states = await service.getStates();
+
+            return new CommandResult<List<string>>(true, "success", states.Keys.ToList());
+        }
+
+        [HttpGet("GetCitiesByStateId")]
+        public async Task<CommandResult<List<string>>> GetCitiesByStateId(string state)
+        {
+            DepartmentService service = new DepartmentService();
+            Dictionary<string, string[]> states = await service.getStates();
+            if(states.ContainsKey(state))
+            {
+                return new CommandResult<List<string>>(true, "success", states[state].ToList());
+            }
+
+            return new CommandResult<List<string>>(false, "El departamento no existe", null);
+        }
+    }
+}
---
+++ prueba_sigma/prueba_sigma/Services/DepartmentService.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace prueba_sigma.Services
+{
+    public class DepartmentService
+    {
+        public async  Task<Dictionary<string, string[]>> getStates()
+        {
+            string endpoint = "https://sigma-studios.s3-us-west-2.amazonaws.com/test/colombia.json";
+            using (var client = new HttpClient())
+            {
+                using (var response = await client.GetAsync($"{endpoint}"))
+                {
+                    var status = response.StatusCode;
+                    if (status == HttpStatusCode.NotFound)
+                    {
+                        return null;
+                    }
+                    else if (status == HttpStatusCode.OK)
+                    {
+                        response.EnsureSuccessStatusCode();
+                        var obj = JsonConvert.DeserializeObject<Dictionary<string, string[]>>(await response.Content.ReadAsStringAsync());
+                        return obj;
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
