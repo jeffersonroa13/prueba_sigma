5b63711ded2756e7f03232a2541f3ad761a76357
Jeff <jefferson.roa@unillanos.edu.co>
2020-08-12 16:38:30 -0500

    creación de modelo y controller para usuario

---
+++ prueba_sigma/prueba_sigma/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace prueba_sigma.Models
+{
+    [Table("contacts")]
+    public class User
+    {
+        [Key]
+        public int Id { get; set;  }
+        public string Name { get; set;  }
+        public string Email { get; set; }
+        public bool State { get; set;  }
+        public string City { get; set;  }
+
+    }
+}
